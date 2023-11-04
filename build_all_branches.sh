82d83bf2ab6a523670b01fd1cc155da7d3e5d48b
Sahan Rasanjana <sahan.user@gmail.com>
2023-11-04 21:54:38 +0530

    website

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -9,8 +9,6 @@ EOF
 touch base/.nojekyll
 
 # Generating documentation for each other branch in a subdirectory
-echo "All branches:"
-echo "$(git branch --remotes --format '%(refname:lstrip=3)' | grep -Ev '^(HEAD|develop|gh-pages)$')"
 for BRANCH in $(git branch --remotes --format '%(refname:lstrip=3)' | grep -Ev '^(HEAD|develop|gh-pages)$'); do
     SANITIZED_BRANCH="$(echo $BRANCH | sed 's/\//_/g')"
     echo "$SANITIZED_BRANCH" >> base/versions.txt
